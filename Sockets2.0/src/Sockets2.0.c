2663275271bf3e3ea0c11c25c6476cabf1227f18
MatiasBerardi <37780250+MatiasBerardi@users.noreply.github.com>
2019-04-11 12:07:47 -0300

    Intento de creacion de Sockets

---
+++ Sockets2.0/src/Sockets2.0.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+int main(void) {
+	//Crear un socket
+		int socketNetwork;
+		socketNetwork = socket(AF_INET, SOCK_STREAM, 0);
+
+
+		//Especificar la dirreccion para el socket
+		struct sockddr_in dirreccionServidor;
+		dirreccionServidor.sin_family = AF_INET;
+		dirreccionServidor.sin_port = htons(9999);
+		dirreccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1");;
+
+
+
+
+		return 0;
+}
