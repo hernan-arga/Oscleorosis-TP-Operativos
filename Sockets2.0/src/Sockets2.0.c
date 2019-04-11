0988d7119ac1595de08e7b825bd1e1162edcf160
abrildelburgo <m.abrildelburgo@hotmail.com>
2019-04-11 15:15:57 -0300

    sockets cliente

--- Sockets2.0/src/Sockets2.0.c
+++ Sockets2.0/src/Sockets2.0.c
@@ -1,3 +1,10 @@
+/*
+ * \\ socket.c \\
+ *
+ *  Created on: 11 abr. 2019
+ *      Author: utnso
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -6,19 +13,45 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 
-int main(void) {
-	//Crear un socket
-		int socketNetwork;
-		socketNetwork = socket(AF_INET, SOCK_STREAM, 0);
+#include <commons/config.h>
 
 
-		//Especificar la dirreccion para el socket
-		struct sockddr_in dirreccionServidor;
-		dirreccionServidor.sin_family = AF_INET;
-		dirreccionServidor.sin_port = htons(9999);
-		dirreccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+int main(void)
+{
+    //crea un socket
+    int socketNetwork;
+    socketNetwork = socket(AF_INET, SOCK_STREAM, 0);
 
+    //especifica la dirreccion para el socket
+    struct sockddr_in direccionServidor;
+    direccionServidor.sin_family = AF_INET;
+    direccionServidor.sinport = htons(9999);
+    direccionServidor.sin_addr.s_addr = INADDR_ANY;
 
+    //establece la conexion
+    int connection_status = connect(socketNetwork, (void*) &direccionServidor, sizeof(direccionServidor));
+    if(connection_status != 0){
+    	perror("NO SE PUDO CONECTAR");
+    	return 1;
+    }
 
-		return 0;
+    //para enviar mensaje al servidor
+    while(1){
+    	char message[500];
+    	scanf("%s", message);
+
+    	send(socketNetwork, message, strlen(message), 0);
+    }
+
+    //recibe mensaje del servidor
+    char server_response[500];
+    recv(socketNetwork, &server_response, sizeof(server_response), 0);
+    printf("EL SERVER MANDO : %s\n", server_response);
+
+    //cerramos socket
+    close(sock);
+
+    return 0;
 }
+
+
