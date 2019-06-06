f4bd08e9c19b7394ab4ac633f6f59ae1b04f666f
Hernan <hernan_rodriguez_2013@hotmail.com>
2019-06-06 11:10:17 -0300

    Arreglando merge conflict

--- Kernel/Kernel.c
+++ Kernel/Kernel.c
@@ -11,15 +11,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <readline/readline.h>
-
-struct Kernel_config{
-	char ip[20];
-	int puerto_memoria;
-	int quantum;
-	int multiprocesamiento;
-	int refresh_metadata;
-	int retardo_ciclo_ejecucion;
-};
+#include <commons/collections/queue.h>
+#include <commons/string.h>
+#include <commons/collections/list.h>
+#include <commons/config.h>
 
 struct LQL{
 	FILE*archivo = NULL;
@@ -41,9 +36,225 @@ struct Script{
 };
 
 typedef enum {
-	SELECT, INSERT, CREATE, DESCRIBE, DROP, OPERACIONINVALIDA
+	SELECT, INSERT, CREATE, DESCRIBE, DROP, JOURNAL, ADD, RUN, METRICS, OPERACIONINVALIDA
 } OPERACION;
 
+void configurar_kernel();
+void dispatcher(t_list*,t_queue*,t_queue*);
+void newbie(FILE*,t_list *,t_queue*);
+int get_PID(t_list *);
+int PID_usada(int,t_list *);
+void pasar_a_ready(t_queue*,t_queue*);
+void tomar_peticion(char* mensaje);
+void realizar_peticion(char**);
+OPERACION tipo_de_peticion(char*);
+int cantidadValidaParametros(char**, int);
+int parametrosValidos(int, char**, int (*criterioTiposCorrectos)(char**, int));
+int esUnNumero(char* cadena);
+int cantidadDeElementosDePunteroDePunterosDeChar(char** puntero);
+int esUnTipoDeConsistenciaValida(char*);
+
+int main(){
+	t_list * PIDs = list_create();
+	t_queue* new = queue_create();
+	t_queue* ready = queue_create();
+
+	FILE* memorias =fopen("IP_MEMORIAS","w");
+	printf("\tKERNEL OPERATIVO Y EN FUNCIONAMIENTO.\n");
+	//configurar_kernel();
+	//operacion_gossiping(memorias); //Le pide a la memoria principal, las ip de las memorias conectadas y las escribe en el archivo IP_MEMORIAS
+	while(1){
+		printf("Mis subprocesos estan a la espera de su mensaje, usuario.\n");
+		char* mensaje = malloc(100);
+		fgets(mensaje,100,stdin);
+		tomar_peticion(mensaje);
+	}
+	dispatcher(PIDs,new,ready);
+	fclose(memorias);
+	return 0;
+}
+
+void tomar_peticion(char* mensaje){
+	//Fijarse despues cual seria la cantidad correcta de malloc
+	char** mensajeSeparado = malloc(strlen(mensaje) + 1);
+	mensajeSeparado = string_split(mensaje, " \n");
+	realizar_peticion(mensajeSeparado);
+	free(mensajeSeparado);
+}
+
+void realizar_peticion(char** parametros) {
+	char *peticion = parametros[0];
+	OPERACION instruccion = tipo_de_peticion(peticion);
+	switch (instruccion) {
+	case SELECT:
+		printf("Seleccionaste Select\n");
+		//Defino de que manera van a ser validos los parametros del select y luego paso el puntero de dicha funcion.
+		//Los parametros son validos si el segundo (la key) es un numero, y la cantidadDeParametrosUsados solo se pasa para hacer
+		//polimorfica la funcion criterioTiposCorrectos.
+		int criterioSelect(char** parametros, int cantidadDeParametrosUsados) {
+			char* key = parametros[2];
+			if (!esUnNumero(key)) {
+				printf("La key debe ser un numero.\n");
+			}
+			return esUnNumero(key);
+		}
+
+		if (parametrosValidos(2, parametros, (void*) criterioSelect)) {
+			printf("Enviando SELECT a memoria.\n");
+		}
+
+		break;
+
+	case INSERT:
+		printf("Seleccionaste Insert\n");
+		int criterioInsert(char** parametros, int cantidadDeParametrosUsados) {
+			char* key = parametros[2];
+			if (!esUnNumero(key)) {
+				printf("La key debe ser un numero.\n");
+			}
+
+			if (cantidadDeParametrosUsados == 4) {
+				char* timestamp = parametros[4];
+				if (!esUnNumero(timestamp)) {
+					printf("El timestamp debe ser un numero.\n");
+				}
+				return esUnNumero(key) && esUnNumero(timestamp);
+			}
+			return esUnNumero(key);
+		}
+		//puede o no estar el timestamp
+		if (parametrosValidos(4, parametros, (void *) criterioInsert)) {
+			printf("Envio el comando INSERT a memoria");
+
+		} else if (parametrosValidos(3, parametros, (void *) criterioInsert)) {
+			printf("Envio el comando INSERT a memoria");
+		}
+		break;
+	case CREATE:
+		printf("Seleccionaste Create\n");
+		int criterioCreate(char** parametros, int cantidadDeParametrosUsados) {
+			char* tiempoCompactacion = parametros[4];
+			char* cantidadParticiones = parametros[3];
+			char* consistencia = parametros[2];
+			if (!esUnNumero(cantidadParticiones)) {
+				printf("La cantidad de particiones debe ser un numero.\n");
+			}
+			if (!esUnNumero(tiempoCompactacion)) {
+				printf("El tiempo de compactacion debe ser un numero.\n");
+			}
+			return esUnNumero(cantidadParticiones)
+					&& esUnNumero(tiempoCompactacion)
+					&& esUnTipoDeConsistenciaValida(consistencia);
+		}
+		if (parametrosValidos(4, parametros, (void *) criterioCreate)) {
+			printf("Enviando CREATE a memoria.\n");
+		}
+		break;
+	default:
+		printf("Error operacion invalida\n");
+	}
+}
+
+int esUnTipoDeConsistenciaValida(char* cadena) {
+	int consistenciaValida = !strcmp(cadena, "SC") || !strcmp(cadena, "SHC")
+			|| !strcmp(cadena, "EC");
+	if (!consistenciaValida) {
+		printf(
+				"El tipo de consistencia no es valida. Asegurese de que este en mayusculas\n");
+	}
+	return consistenciaValida;
+}
+
+int parametrosValidos(int cantidadDeParametrosNecesarios, char** parametros,
+		int (*criterioTiposCorrectos)(char**, int)) {
+	return cantidadValidaParametros(parametros, cantidadDeParametrosNecesarios)
+			&& criterioTiposCorrectos(parametros,
+					cantidadDeParametrosNecesarios);;
+}
+
+int cantidadValidaParametros(char** parametros,
+		int cantidadDeParametrosNecesarios) {
+	//Saco de la cuenta la peticion y el NULL
+	int cantidadDeParametrosQueTengo =
+			cantidadDeElementosDePunteroDePunterosDeChar(parametros) - 2;
+	if (cantidadDeParametrosQueTengo != cantidadDeParametrosNecesarios) {
+		//hay que arreglar esto para que en el caso de insert solo lo muestre si no se cumple con 4 ni con 3
+		printf("La cantidad de parametros no es valida\n");
+		return 0;
+	}
+	return 1;
+}
+
+int esUnNumero(char* cadena) {
+	for (int i = 0; i < strlen(cadena); i++) {
+		if (!isdigit(cadena[i])) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int cantidadDeElementosDePunteroDePunterosDeChar(char** puntero) {
+	int i = 0;
+	while (puntero[i] != NULL) {
+		i++;
+	}
+	//Uno mas porque tambien se incluye el NULL en el vector
+	return ++i;
+}
+
+OPERACION tipo_de_peticion(char* peticion) {
+	string_to_upper(peticion);
+	if (!strcmp(peticion, "SELECT")) {
+		free(peticion);
+		return SELECT;
+	} else {
+		if (!strcmp(peticion, "INSERT")) {
+			free(peticion);
+			return INSERT;
+		} else {
+			if (!strcmp(peticion, "CREATE")) {
+				free(peticion);
+				return CREATE;
+			} else{
+				if(!strcmp(peticion,"DESCRIBE")){
+					free(peticion);
+					return DESCRIBE;
+				}else{
+					if (!strcmp(peticion, "DROP")) {
+						free(peticion);
+						return DROP;
+					} else{
+						if (!strcmp(peticion, "JOURNAL")) {
+							free(peticion);
+							return JOURNAL;
+						} else{
+							if (!strcmp(peticion, "ADD")) {
+								free(peticion);
+								return ADD;
+							} else{
+								if (!strcmp(peticion, "RUN")) {
+									free(peticion);
+									return RUN;
+								} else{
+									if (!strcmp(peticion, "METRICS")) {
+										free(peticion);
+										return METRICS;
+										} else{
+											free(peticion);
+											return OPERACIONINVALIDA;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+}
+
+/*
 int main()
 {
 	printf("Soy Kernel \n");
@@ -79,6 +290,7 @@ int main()
 
 	return 0;
 }
+*/
 
 
 void menu(){
@@ -277,57 +489,100 @@ int tiposCorrectos(char* parametros, int cantidadDeParametrosNecesarios, int (*c
 			cantidadDeParametrosNecesarios);
 }
 
-int cantidadValidaParametros(char* parametros, int cantidadDeParametrosNecesarios) {
-	char parametrosSeparados[cantidadDeParametrosNecesarios][30];
-	int cantidadParametrosValida = separarEnVector(&parametros,
-			parametrosSeparados, cantidadDeParametrosNecesarios);
-	if (!cantidadParametrosValida)
-		//hay que arreglar esto para que en el caso de insert solo lo muestre si no se cumple con 4 ni con 3
-		printf("La cantidad de parametros no es valida\n");
-	return cantidadParametrosValida;
+void configurar_kernel(){
+	FILE* archivo_configuracion = fopen("Kernel_config","w");
+	t_config* configuracion = config_create("Kernel_config");
+	printf("\tQue bien! Parece que hoy van a configurarme\n");
+	char * valor = string_new();
+	printf("\tNecesito una direccion IP, asi podre comunicarme con mis preciosas memorias.\n Por favor ingresa mi IP\n");
+	fgets(valor, 100, stdin);
+	config_set_value(configuracion,"IP",valor);
+	printf("\tGracias, ahora me dieron ganas de hablar con las memorias... Por un canal privado. Podrias conseguirme un puerto?\n Por favor, ingresa el puerto para comunicarme con las memorias\n");
+	fgets(valor, 100, stdin);
+	config_set_value(configuracion,"PUERTO_MEMORIA",valor);
+	printf("\tExcelente! Pero hay otro problema: Esos request no se van a ejecutar en un FIFO arcaico, no. Tenemos un RoundRobin!\n Por favor, ingrese el numero de quantum: \n");
+	fgets(valor, 100, stdin);
+	config_set_value(configuracion,"QUANTUM",valor);
+	printf("\tLo siento, se que es engorroso... Pero para empezar, fuiste vos el que inicio mi ejecucion.\n Ahora necesito que me digas cuantos procesos van a estar ejecutandose a la vez en las memorias\n Ingresa el grado de multiprocesamiento: \n");
+	fgets(valor, 100, stdin);
+	config_set_value(configuracion,"MULTIPROCESAMIENTO",valor);
+	printf("\tTodavia no se ni que es eso, pero por las dudas pone algun numerito...\n Ingresa el numero de Fresh Metadata: \n");
+	fgets(valor, 100, stdin);
+	config_set_value(configuracion,"METADATA_REFRESH",valor);
+	printf("\tMuy bien, por ultimo necesito otro numero que se mide en milisegundos, que rapido!\n Ingresa el retardo del ciclo de ejecucion: \n");
+	fgets(valor, 100, stdin);
+	config_set_value(configuracion,"SLEEP_EJECUCION",valor);
+	printf("\tBueno, eso es todo. Esperame que guardo estos datos en mi archivo de configuracion\n");
+	config_save(configuracion);
+	fclose(archivo_configuracion);
+	config_destroy(configuracion);
+	}
+
+void dispatcher(t_list* PIDs,t_queue* new,t_queue* ready){
+	char * nombre_del_archivo = malloc(100);
+	printf("\tNecesito que ingreses el nombre del archivo a leer: ");
+	fgets(nombre_del_archivo,100,stdin);
+	printf("%s",nombre_del_archivo);
+	string_trim(&nombre_del_archivo);
+	if(!string_contains(nombre_del_archivo,".lql")){
+		string_append(&nombre_del_archivo,".lql");
+	printf("%s",nombre_del_archivo);
+	FILE* archivo = fopen(nombre_del_archivo,"r");
+	if(archivo==NULL){
+		printf("El archivo no existe\n");
+	}
+	else{
+			newbie(archivo,PIDs,new);
+			pasar_a_ready(new,ready);
+	}
+	fclose(archivo);
+	free(nombre_del_archivo);
+	}
 }
 
-OPERACION tipoDePeticion(char* peticion) {
-	char* peticionUpperCase = malloc(strlen(peticion)+1);
-	stringToUpperCase(peticion, &peticionUpperCase);
-	if (!strcmp(peticionUpperCase, "SELECT")) {
-		free(peticionUpperCase);
-		return SELECT;
-	} else {
-		if (!strcmp(peticionUpperCase, "INSERT")) {
-			free(peticionUpperCase);
-			return INSERT;
-		} else {
-			if (!strcmp(peticionUpperCase, "CREATE")) {
-				free(peticionUpperCase);
-				return CREATE;
-			} else {
-				free(peticionUpperCase);
-				return OPERACIONINVALIDA;
-			}
+
+void newbie(FILE* archivo, t_list * PIDs,t_queue* new){ //Prepara las estructuras necesarias y pushea el request a la cola de new
+	 printf("\tAgregando script a cola de New\n");
+	 script* proceso = malloc(sizeof(script));
+	 proceso->PID = get_PID(PIDs);
+	 proceso->PC = 0;
+	 proceso->peticiones = archivo;
+	 queue_push(new,proceso);
+	 printf("Script agregado\n");
+}
+
+int get_PID(t_list * PIDs){
+	int PID = 1;
+	int flag = 0;
+	do{
+		if(PID_usada(PID,PIDs)){
+			PID++;
 		}
-	}
-}
-
-int esUnNumero(char* cadena) {
-	for (int i = 0; i < strlen(cadena); i++) {
-		if (!isdigit(cadena[i])) {
-			return 0;
+		else{
+			flag = 1;
 		}
-	}
-	return 1;
+	}while(flag == 0);
+	list_add(PIDs, &PID);
+	return PID;
 }
 
-void stringToUpperCase(char* palabra, char** palabraEnMayusculas) {
-	char* aux = malloc(strlen(palabra)+1);
-	strcpy(aux, palabra);
-	int i = 0;
-	while (aux[i] != '\0') {
-		aux[i] = toupper(aux[i]);
-		i++;
+
+
+int PID_usada(int numPID,t_list * PIDs){
+	bool _PID_en_uso(void* PID){
+		return (int)PID == numPID;
 	}
-	strcpy(*palabraEnMayusculas, aux);
-	free(aux);
+	return (list_find(PIDs,_PID_en_uso) != NULL);
 }
 
+void pasar_a_ready(t_queue* new,t_queue* ready){
+	printf("\tTrasladando script de New a Ready\n");
+	queue_push(ready,queue_pop(new));
+	 printf("Script trasladado a Ready\n");
+}
+
+//void gossiping()
+
+//TAREA:
+//Crear un planificador de RR con quantum configurable y que sea capaz de parsear los archivos LQL
 //Lista de programas activos con PID cada uno, si alguno se termina de correr, el PID vuelve a estar libre para que otro programa entrante lo ocupe.*/
--- LFS/LFS.c
+++ LFS/LFS.c
@@ -580,11 +580,203 @@ int existeCarpeta(char *nombreCarpeta) {
 			closedir(directorio);
 			return 1;
 		}
+<<<<<<< HEAD
+=======
+
+		int timestampActualMayorBloques = -1;
+		char* valueDeTimestampActualMayorBloques = string_new();
+
+		// POR CADA BLOQUE, TENGO QUE ENTRAR A ESTE BLOQUE
+		for(int i=0; i< m; i++){
+			char* pathBloque = malloc(strlen("./Bloques/") + strlen((vectorBloques[i])) + strlen(".bin") + 1);
+			strcpy(pathBloque, "./Bloques/");
+			strcat(pathBloque, vectorBloques[i]);
+			strcat(pathBloque, ".bin");
+			FILE *archivoBloque = fopen(pathBloque, "r");
+			if (archivoBloque == NULL) {
+				printf("no se pudo abrir archivo de bloques");
+				exit(1);
+			}
+
+			int cantidadIgualDeKeysEnBloque = 0;
+			t_registro* vectorStructs[100];
+			obtenerDatosParaKeyDeseada(archivoBloque, (atoi(key)), vectorStructs, &cantidadIgualDeKeysEnBloque);
+
+			printf("%i", vectorStructs[0]->timestamp);
+			printf("%i", vectorStructs[1]->timestamp);
+
+			//cual de estos tiene el timestamp mas grande? guardar timestamp y value
+			int temp = 0;
+			char* valor;
+			for (int k = 1; k < cantidadIgualDeKeysEnBloque; k++){
+				for (int j = 0; j < (cantidadIgualDeKeysEnBloque-k); j++){
+						if (vectorStructs[j]->timestamp < vectorStructs[j+1]->timestamp){
+							temp = vectorStructs[j+1]->timestamp;
+							valor = malloc(strlen(vectorStructs[j+1]->value));
+							strcpy(valor,vectorStructs[j+1]->value);
+
+							vectorStructs[j+1]->timestamp = vectorStructs[j]->timestamp;
+							vectorStructs[j+1]->value = vectorStructs[j]->value;
+
+							vectorStructs[j]->timestamp = temp;
+							vectorStructs[j]->value = valor;
+					}
+			    }
+			 } // aca quedaria el vector ordenado por timestamp mayor
+			if(vectorStructs[0]->timestamp > timestampActualMayorBloques){
+				timestampActualMayorBloques = vectorStructs[0]->timestamp;
+				strcpy(valueDeTimestampActualMayorBloques, "");
+				string_append(&valueDeTimestampActualMayorBloques, vectorStructs[0]->value);
+			}
+			fclose(archivoBloque);
+			free(pathBloque);
+			//free(vectorStructs);
+		} 	//cierra el for
+
+		// si encontro alguno, me guarda el timestamp mayor en timestampActualMayorBloques
+		// y guarda el valor en valueDeTimestampActualMayorBloques
+		// si no hay ninguno en vectorBloques (porque por ej, esta en los temporales)
+		// entonces timestampActualMayorBloques = -1 y
+		// valueDeTimestampActualMayorBloques = NULL
+
+		//-------------------------------------------------
+
+		// AHORA ABRO ARCHIVOS TEMPORALES. EL PROCEDIMIENTO ES MUY PARECIDO AL ANTERIOR
+
+		char* pathTemporales = malloc(strlen("./Tables/") + strlen(tabla) + 1);
+		strcpy(pathTemporales, "./Tables/");
+		strcat(pathTemporales, tabla);
+
+		DIR *directorioTemporal = opendir(pathTemporales);
+		struct dirent *archivoALeer;
+
+		int timestampActualMayorTemporales = -1;
+		char* valueDeTimestampActualMayorTemporales = string_new();
+
+		while((archivoALeer = readdir(directorioTemporal)) != NULL) { //PARA CADA ARCHIVO DE LA TABLA ESPECIFICA
+			if( string_ends_with(archivoALeer->d_name, ".tmp") ){
+
+				//obtengo el nombre de ese archivo .tmp . Ejemplo obtengo A1.tmp siendo A1 el nombre (tipo char*)
+				char* nombreArchivoTemporal = string_split( archivoALeer->d_name, ".")[0];
+				// ahora ya tengo el nombre del archivo .tmp
+
+				char* pathTemporal = malloc(strlen("./Tables/") + strlen(tabla) + strlen("/") + strlen( nombreArchivoTemporal ) + strlen(".tmp") + 1);
+				strcpy(pathTemporal, "./Tables/");
+				strcat(pathTemporal, tabla);
+				strcat(pathTemporal, "/");
+				strcat(pathTemporal, nombreArchivoTemporal );
+				strcat(pathTemporal, ".tmp");
+				FILE *fileTemporal = fopen(pathTemporal, "r");
+				if (fileTemporal == NULL) {
+					printf("no se pudo abrir archivo de temporales");
+					exit(1);
+				}
+
+				t_config *tamanioYBloquesTmp = config_create(pathTemporal);
+				char** vectorBloquesTmp = config_get_array_value(tamanioYBloquesTmp, "BLOCK"); //devuelve vector de STRINGS
+
+				int n = 0;
+				while(vectorBloquesTmp[n] != NULL){
+					n ++;
+				}
+
+				//POR CADA BLOQUE, TENGO QUE ENTRAR A ESE BLOQUE
+				for(int q=0; q< n; q++){
+					char* pathBloqueTmp = malloc(strlen("./Bloques/") + strlen((vectorBloques[q])) + strlen(".bin") + 1);
+					strcpy(pathBloqueTmp, "./Bloques/");
+					strcat(pathBloqueTmp, vectorBloquesTmp[q]);
+					strcat(pathBloqueTmp, ".bin");
+					FILE *archivoBloqueTmp = fopen(pathBloqueTmp, "r");
+					if (archivoBloqueTmp == NULL) {
+						printf("no se pudo abrir archivo de bloques");
+						exit(1);
+					}
+
+					int cantidadIgualDeKeysEnTemporal = 0;
+					t_registro* vectorStructsTemporal[100];
+					obtenerDatosParaKeyDeseada(archivoBloqueTmp, (atoi(key)), vectorStructsTemporal, &cantidadIgualDeKeysEnTemporal);
+
+					//cual de estos tiene el timestamp mas grande? guardar timestamp y value
+					int tempo = 0;
+					char* valorTemp;
+					for (int k = 1; k < cantidadIgualDeKeysEnTemporal; k++){
+						for (int j = 0; j < (cantidadIgualDeKeysEnTemporal-k); j++){
+							if (vectorStructsTemporal[j]->timestamp < vectorStructsTemporal[j+1]->timestamp){
+								tempo = vectorStructsTemporal[j+1]->timestamp;
+								valorTemp = malloc(strlen(vectorStructsTemporal[j+1]->value));
+								strcpy(valorTemp,vectorStructsTemporal[j+1]->value);
+
+								vectorStructsTemporal[j+1]->timestamp = vectorStructsTemporal[j]->timestamp;
+								vectorStructsTemporal[j+1]->value = vectorStructsTemporal[j]->value;
+
+								vectorStructsTemporal[j]->timestamp = tempo;
+								vectorStructsTemporal[j]->value = valorTemp;
+							}
+						}
+					}
+
+					if(vectorStructsTemporal[0]->timestamp > timestampActualMayorTemporales){
+						timestampActualMayorTemporales = vectorStructsTemporal[0]->timestamp;
+						strcpy(valueDeTimestampActualMayorTemporales, "");
+						string_append(&valueDeTimestampActualMayorTemporales, vectorStructsTemporal[0]->value);
+					}
+					fclose(archivoBloqueTmp);
+					free(pathBloqueTmp);
+					//free(vectorStructsTemporal);
+				} // cierra el for      */
+			} // cierra el if
+		} //cierra el while
+
+		// si encontro alguno, me guarda el timestamp mayor en timestampActualMayorTemporales
+		// y guarda el valor en valueDeTimestampActualMayorTemporales
+		// si no hay ninguno en vectorStructsTemporal
+		// entonces timestampActualMayorTemporales = -1 y
+		// valueDeTimestampActualMayorTemporales = NULL
+
+		closedir(directorioTemporal);
+
+		// ----------------------------------------------------
+
+
+		// aca iria verificar los datos tamb de la memoria de la tabla
+
+		//-----------------------------------------------------
+
+		if(timestampActualMayorBloques >= timestampActualMayorTemporales){
+			printf("%s\n", valueDeTimestampActualMayorBloques);
+		}
+		else{
+			printf("%s\n", valueDeTimestampActualMayorTemporales);
+		}
+
+		free(pathMetadata);
+		free(pathParticionQueContieneKey);
+		free(pathTemporales);
+		//free(pathTemporal);
+		config_destroy(tamanioYBloques);
+		config_destroy(metadata);
+
+
+	// SI NO ENCUENTRA LA TABLA (lo de abajo)
+	} else {
+		char* mensajeALogear = malloc(
+				strlen("Error: no existe una tabla con el nombre ")
+						+ strlen(tabla) + 1);
+		strcpy(mensajeALogear, "Error: no existe una tabla con el nombre ");
+		strcat(mensajeALogear, tabla);
+		t_log* g_logger;
+		//Si uso LOG_LEVEL_ERROR no lo imprime ni lo escribe
+		g_logger = log_create("./erroresSelect.log", "LFS", 1, LOG_LEVEL_INFO);
+		log_info(g_logger, mensajeALogear);
+		log_destroy(g_logger);
+		free(mensajeALogear);
+>>>>>>> 67f1160bc210ce7b9045e4cb9b58d52f1b28ba84
 	}
 	closedir(directorio);
 	return 0;
 }
 
+<<<<<<< HEAD
 //No le pongo "select" porque ya esta la funcion de socket y rompe
 
 void obtenerDatosParaKeyDeseada(FILE *archivoBloque, int key,
@@ -595,6 +787,52 @@ void obtenerDatosParaKeyDeseada(FILE *archivoBloque, int key,
 	while (fgets(linea, 50, archivoBloque) != NULL) {
 		int keyLeida = atoi(string_split(linea, ";")[1]);
 		if (keyLeida == key) {
+=======
+void obtenerDatosParaKeyDeseada(FILE *fp, int key, t_registro** vectorStructs, int *cant){
+	char linea[50];
+	int i = 0;
+
+	    char * line = NULL;
+	    size_t len = 0;
+	    ssize_t read;
+
+	    while ((read = getline(&line, &len, fp)) != -1) {
+			int keyLeida = atoi(string_split(line,";")[1]);
+			if(keyLeida == key){
+				t_registro* p_registro = malloc(12); // 2 int = 2* 4        +       un puntero a char = 4
+				t_registro p_registro2;
+				p_registro = &p_registro2;
+				char** arrayLinea = malloc(strlen(line) + 1);
+				arrayLinea = string_split(line, ";");
+				int timestamp = atoi(arrayLinea[0]);
+				int key = atoi(arrayLinea[1]);
+				p_registro->timestamp = timestamp;
+				p_registro->key = key;
+				p_registro->value = malloc(strlen(arrayLinea[2]));
+
+				strcpy(p_registro->value,arrayLinea[2]);
+				vectorStructs[i] = p_registro;
+				//vectorStructs[i]->key = p_registro->key;
+				//vectorStructs[i]->timestamp = p_registro->timestamp;
+
+				i++;
+				(*cant)++;
+
+				//int untime = vectorStructs[0]->timestamp;
+
+	    }
+	    }
+
+	    printf("%i", vectorStructs[0]->timestamp);
+	    printf("%i", vectorStructs[1]->timestamp);
+
+
+	/*
+
+	while( fgets(linea,50,archivoBloque) != NULL ){
+		int keyLeida = atoi(string_split(linea,";")[1]);
+		if(keyLeida == key){
+>>>>>>> 67f1160bc210ce7b9045e4cb9b58d52f1b28ba84
 			t_registro* p_registro = malloc(12); // 2 int = 2* 4        +       un puntero a char = 4
 			t_registro p_registro2;
 			p_registro = &p_registro2;
@@ -611,7 +849,7 @@ void obtenerDatosParaKeyDeseada(FILE *archivoBloque, int key,
 			i++;
 			(*cant)++;
 		}
-	}
+	}  				*/
 }
 
 /*
